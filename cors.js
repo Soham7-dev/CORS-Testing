0f71a747c507e5faebcaa28e22f07dda0883e5c8
Soham Das <64075511+Soham7-dev@users.noreply.github.com>
2026-08-24 13:53:03 +0530

    Update API endpoint in cors.js

--- cors.js
+++ cors.js
@@ -1,6 +1,6 @@
 var req = new XMLHttpRequest();
 req.onload = reqListener;
-req.open('get','https://my.cyberghostvpn.com/subscriptions/13265986/bills',true);
+req.open('get','https://api.cyberghostvpn.com/cg/status?flags=0&os=win&os_version=10.0.26200&app_version=8.4.15.14706&app_language=en&os_region=us&environment=Live&affiliate=&partners_id=1',true);
 req.withCredentials = true;
 req.send();
 
